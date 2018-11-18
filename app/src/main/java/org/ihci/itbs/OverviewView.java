37e1bc28b8a97c28e46bcc8c180f8d352d6ce604
Yukino0812 <Yukino0812@qq.com>
2018-11-18 20:53:21 +0800

    Initial commit

---
+++ app/src/main/java/org/ihci/itbs/OverviewView.java
@@ -0,0 +1,16 @@
+package org.ihci.itbs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OverviewView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overview_view);
+    }
+
+    
+
+}
