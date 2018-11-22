5764cfa6d753713a25055c16a84b0f7b55ac031c
Yukino0812 <Yukino0812@qq.com>
2018-11-22 23:36:20 +0800

    persistent

--- app/src/main/java/org/ihci/itbs/model/GlobalSettingModel.java
+++ app/src/main/java/org/ihci/itbs/model/GlobalSettingModel.java
@@ -1,6 +1,19 @@
 package org.ihci.itbs.model;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import org.ihci.itbs.view.ItbsApplication;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Yukino Yukinoshita
@@ -20,6 +33,9 @@ public class GlobalSettingModel implements Serializable {
 
     public static GlobalSettingModel getInstance() {
         if (INSTANCE == null) {
+            read();
+        }
+        if (INSTANCE == null) {
             INSTANCE = new GlobalSettingModel();
         }
         return INSTANCE;
@@ -31,6 +47,7 @@ public class GlobalSettingModel implements Serializable {
 
     public void setCurrentUserName(String currentUserName) {
         this.currentUserName = currentUserName;
+        save();
     }
 
     public UiTheme getCurrentTheme() {
@@ -39,6 +56,7 @@ public class GlobalSettingModel implements Serializable {
 
     public void setCurrentTheme(UiTheme currentTheme) {
         this.currentTheme = currentTheme;
+        save();
     }
 
     public boolean isRecommendSetting() {
@@ -47,9 +65,55 @@ public class GlobalSettingModel implements Serializable {
 
     public void setRecommendSetting(boolean recommendSetting) {
         this.recommendSetting = recommendSetting;
+        save();
     }
 
-    public enum UiTheme{
+    private static void save() {
+        final String fileName = "global_setting_model.txt";
+
+        ThreadPoolExecutor singleThreadForSave = new ThreadPoolExecutor(1, 1, 3, TimeUnit.SECONDS,
+                new ArrayBlockingQueue(5),
+                new ThreadFactory() {
+                    @Override
+                    public Thread newThread(@NonNull Runnable r) {
+                        return new Thread(r, "GlobalSettingModel Single Thread For Save");
+                    }
+                });
+
+        singleThreadForSave.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ObjectOutputStream objectOutputStream = new ObjectOutputStream(ItbsApplication.getContext().openFileOutput(fileName, Context.MODE_PRIVATE));
+                    objectOutputStream.writeObject(getInstance());
+                    objectOutputStream.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    private static void read() {
+        final String fileName = "global_setting_model.txt";
+
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(ItbsApplication.getContext().openFileInput(fileName));
+            INSTANCE = (GlobalSettingModel) objectInputStream.readObject();
+            objectInputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public enum UiTheme {
 
         /**
          * UI theme boy style
