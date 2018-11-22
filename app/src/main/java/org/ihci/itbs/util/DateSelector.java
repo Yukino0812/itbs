cd6dc5f0db9cadbb14f2d63988a746c354e29cb1
Yukino0812 <Yukino0812@qq.com>
2018-11-22 22:58:14 +0800

    add util DateSelector

---
+++ app/src/main/java/org/ihci/itbs/util/DateSelector.java
@@ -0,0 +1,179 @@
+package org.ihci.itbs.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * This {@code DateSelector} utility is used for select a {@code Date}
+ * quickly and conveniently.
+ * <p>
+ * Try not to handle {@code Date} directly in other classes.
+ *
+ * @author Yukino Yukinoshita
+ * @version 1.0
+ */
+
+public class DateSelector {
+
+    private Date currentDate;
+
+    public DateSelector() {
+        currentDate = new Date();
+    }
+
+    public DateSelector(Date currentDate) {
+        setCurrentDate(currentDate);
+    }
+
+    public Date getCurrentDate() {
+        return currentDate;
+    }
+
+    public void setCurrentDate(Date currentDate) {
+        this.currentDate = currentDate;
+    }
+
+    /**
+     * Get several days after
+     *
+     * @param daysAfter number of day after
+     * @return the day after {@code daysAfter} days
+     */
+    public Date getDaysAfter(int daysAfter) {
+        if (currentDate == null) {
+            currentDate = new Date();
+        }
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(currentDate);
+        calendar.add(Calendar.DATE, daysAfter);
+        return calendar.getTime();
+    }
+
+    public static Date getStartTimeThisDay(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar.getTime();
+    }
+
+    public static Date getEndTimeThisDay(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MILLISECOND, 999);
+        return calendar.getTime();
+    }
+
+    /**
+     * Get the first day in this week
+     *
+     * @return first day with time 0:00:00
+     */
+    public static Date getFirstDayCurrentWeek() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.set(Calendar.DAY_OF_WEEK, 2);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        return calendar.getTime();
+    }
+
+    /**
+     * Get the last day in this week
+     *
+     * @return last day with time 0:00:00
+     */
+    public static Date getLastDayCurrentWeek() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(getFirstDayCurrentWeek());
+        calendar.add(Calendar.DATE, 6);
+        return calendar.getTime();
+    }
+
+    /**
+     * Get the last day and time in this week
+     *
+     * @return last day with time 23:59:59
+     */
+    public static Date getLastDayAndTimeCurrentWeek() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(getLastDayCurrentWeek());
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        return calendar.getTime();
+    }
+
+    /**
+     * Get the first day in specified week
+     *
+     * @param specifiedWeek specified week number
+     * @param currentWeek current week number
+     * @return first day with time 0:00:00
+     */
+    public static Date getFirstDaySpecifiedWeek(int specifiedWeek, int currentWeek) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.WEEK_OF_YEAR, specifiedWeek - currentWeek);
+        calendar.set(Calendar.DAY_OF_WEEK, 2);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        return calendar.getTime();
+    }
+
+    /**
+     * Get the last day in specified week
+     *
+     * @param specifiedWeek specified week number
+     * @param currentWeek current week number
+     * @return last day with time 0:00:00
+     */
+    public static Date getLastDaySpecifiedWeek(int specifiedWeek, int currentWeek) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(getFirstDaySpecifiedWeek(specifiedWeek, currentWeek));
+        calendar.add(Calendar.DATE, 6);
+        return calendar.getTime();
+    }
+
+    /**
+     * Get the last day and time in specified week
+     *
+     * @param specifiedWeek specified week number
+     * @param currentWeek current week number
+     * @return last day with time 23:59:59
+     */
+    public static Date getLastDayAndTimeSpecifiedWeek(int specifiedWeek, int currentWeek) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(getLastDaySpecifiedWeek(specifiedWeek, currentWeek));
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        return calendar.getTime();
+    }
+
+    public static String dateToString(Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.CHINA);
+        return simpleDateFormat.format(date);
+    }
+
+    public static Date stringToDate(String date) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.CHINA);
+        return simpleDateFormat.parse(date);
+    }
+
+    @Override
+    public String toString() {
+        return dateToString(getCurrentDate());
+    }
+
+}
