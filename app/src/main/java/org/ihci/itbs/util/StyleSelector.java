0d4eba29f05a6aec3940ad41dc335b560412b49c
Yukino0812 <Yukino0812@qq.com>
2018-11-26 23:32:46 +0800

    select style

---
+++ app/src/main/java/org/ihci/itbs/util/StyleSelector.java
@@ -0,0 +1,93 @@
+package org.ihci.itbs.util;
+
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+
+import org.ihci.itbs.R;
+import org.ihci.itbs.model.GlobalSettingModel;
+import org.ihci.itbs.view.ItbsApplication;
+
+/**
+ * @author Yukino Yukinoshita
+ */
+
+public class StyleSelector {
+
+    public static int getTextColor() {
+        switch (GlobalSettingModel.getInstance().getCurrentTheme()) {
+            case BOY:
+                return Color.BLACK;
+            case GIRL:
+                return Color.WHITE;
+            default:
+                return Color.BLACK;
+        }
+    }
+
+    public static int getColorPrimary() {
+        switch (GlobalSettingModel.getInstance().getCurrentTheme()) {
+            case BOY:
+                return ItbsApplication.getContext().getResources().getColor(R.color.colorBoyPrimary);
+            case GIRL:
+                return ItbsApplication.getContext().getResources().getColor(R.color.colorGirlPrimary);
+            default:
+                return ItbsApplication.getContext().getResources().getColor(R.color.colorBoyPrimary);
+        }
+    }
+
+    public static int getColorLight() {
+        switch (GlobalSettingModel.getInstance().getCurrentTheme()) {
+            case BOY:
+                return ItbsApplication.getContext().getResources().getColor(R.color.colorBoyLight);
+            case GIRL:
+                return ItbsApplication.getContext().getResources().getColor(R.color.colorGirlLight);
+            default:
+                return ItbsApplication.getContext().getResources().getColor(R.color.colorBoyLight);
+        }
+    }
+
+    public static int getColorDeep() {
+        switch (GlobalSettingModel.getInstance().getCurrentTheme()) {
+            case BOY:
+                return ItbsApplication.getContext().getResources().getColor(R.color.colorBoyDeep);
+            case GIRL:
+                return ItbsApplication.getContext().getResources().getColor(R.color.colorGirlDeep);
+            default:
+                return ItbsApplication.getContext().getResources().getColor(R.color.colorBoyDeep);
+        }
+    }
+
+    public static Drawable getDefaultAvatar() {
+        switch (GlobalSettingModel.getInstance().getCurrentTheme()) {
+            case BOY:
+                return ItbsApplication.getContext().getDrawable(R.drawable.user_avatar_default_boy);
+            case GIRL:
+                return ItbsApplication.getContext().getDrawable(R.drawable.user_avatar_default_girl);
+            default:
+                return ItbsApplication.getContext().getDrawable(R.drawable.user_avatar_default_boy);
+        }
+    }
+
+    public static Drawable getTopMouth() {
+        switch (GlobalSettingModel.getInstance().getCurrentTheme()) {
+            case BOY:
+                return ItbsApplication.getContext().getDrawable(R.drawable.logo_boy_top);
+            case GIRL:
+                return ItbsApplication.getContext().getDrawable(R.drawable.logo_girl_top);
+            default:
+                return ItbsApplication.getContext().getDrawable(R.drawable.logo_boy_top);
+        }
+    }
+
+    public static Drawable getGoal() {
+        switch (GlobalSettingModel.getInstance().getCurrentTheme()) {
+            case BOY:
+                return ItbsApplication.getContext().getDrawable(R.drawable.goal_boy);
+            case GIRL:
+                return ItbsApplication.getContext().getDrawable(R.drawable.goal_girl);
+            default:
+                return ItbsApplication.getContext().getDrawable(R.drawable.goal_boy);
+        }
+    }
+
+}
