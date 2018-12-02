372e6d087b4da70d5ff450de3f10a95cdd586c55
Yukino0812 <Yukino0812@qq.com>
2018-12-02 15:16:54 +0800

    check goal finish

--- app/src/main/java/org/ihci/itbs/presenter/GoalPresenter.java
+++ app/src/main/java/org/ihci/itbs/presenter/GoalPresenter.java
@@ -4,8 +4,14 @@ import org.ihci.itbs.contract.GoalContract;
 import org.ihci.itbs.model.GlobalSettingModel;
 import org.ihci.itbs.model.GoalModel;
 import org.ihci.itbs.model.UserModel;
+import org.ihci.itbs.model.pojo.Award;
+import org.ihci.itbs.model.pojo.Currency;
 import org.ihci.itbs.model.pojo.Goal;
+import org.ihci.itbs.model.pojo.HistoryUse;
+import org.ihci.itbs.model.pojo.Toothbrush;
 import org.ihci.itbs.model.pojo.User;
+import org.ihci.itbs.util.DateSelector;
+import org.jetbrains.annotations.Contract;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
@@ -22,10 +28,12 @@ public class GoalPresenter implements GoalContract.Presenter {
 
     private WeakReference<GoalContract.View> viewWeakReference;
     private GoalModel model;
+    private UserModel userModel;
 
     public GoalPresenter(GoalContract.View view) {
         this.viewWeakReference = new WeakReference<>(view);
         model = new GoalModel(this);
+        userModel = new UserModel(this);
     }
 
     @Override
@@ -50,21 +58,84 @@ public class GoalPresenter implements GoalContract.Presenter {
 
     @Override
     public Goal getUserGoal() {
-        UserModel userModel = new UserModel(this);
         User user = userModel.getLocalUser(GlobalSettingModel.getInstance().getCurrentUserName());
-        Goal goal = user.getGoal();
-        if (goal == null) {
+        if (user == null) {
             return null;
         }
-        return model.getGoal(goal.getGoalId());
+        return user.getGoal();
     }
 
     @Override
     public void setUserGoal(Goal goal) {
-        UserModel userModel = new UserModel(this);
         User user = userModel.getLocalUser(GlobalSettingModel.getInstance().getCurrentUserName());
+        goal.setStartDate(new Date());
         user.setGoal(goal);
-        user.getGoal().setStartDate(new Date());
+        userModel.updateUser(user.getUserName(), user);
+    }
+
+    @Override
+    public void checkIsGoalFinish() {
+        Goal goal = getUserGoal();
+        if (goal == null) {
+            return;
+        }
+        int days;
+        switch (goal.getGoalId()) {
+            case 1:
+                days = 1;
+                break;
+            case 2:
+                days = 3;
+                break;
+            case 3:
+                days = 7;
+                break;
+            case 4:
+                days = 15;
+                break;
+            case 5:
+                days = 21;
+                break;
+            case 6:
+                days = 30;
+                break;
+            default:
+                days = 0;
+        }
+        if (days == 0) {
+            return;
+        }
+        if (checkBrushContinuous(goal.getStartDate(), days)) {
+            viewWeakReference.get().notifyGoalFinish(0, goal.getAward());
+        }
+    }
+
+    @Override
+    public void userFinishGoal(int star, Award award) {
+        User user = userModel.getLocalUser(GlobalSettingModel.getInstance().getCurrentUserName());
+        if (user == null) {
+            return;
+        }
+        Currency currency = user.getCurrency();
+        if (currency == null) {
+            currency = new Currency();
+        }
+        int totalStar = currency.getSeniorCurrency() * 3 + currency.getJuniorCurrency();
+        totalStar += star;
+        currency.setSeniorCurrency(totalStar / 3);
+        currency.setJuniorCurrency(totalStar % 3);
+
+        ArrayList<Award> awardList = user.getAwardArrayList();
+        if (awardList == null) {
+            awardList = new ArrayList<>();
+        }
+        if (award != null) {
+            awardList.add(award);
+        }
+
+        user.setGoal(null);
+        user.setCurrency(currency);
+        user.setAwardArrayList(awardList);
         userModel.updateUser(user.getUserName(), user);
     }
 
@@ -77,4 +148,63 @@ public class GoalPresenter implements GoalContract.Presenter {
             }
         });
     }
+
+    @Contract("null, _ -> false")
+    private boolean checkBrushContinuous(Date startDate, int days) {
+        if (startDate == null) {
+            return false;
+        }
+        User user = userModel.getLocalUser(GlobalSettingModel.getInstance().getCurrentUserName());
+        if (user == null) {
+            return false;
+        }
+        ArrayList<Toothbrush> toothbrushes = user.getToothbrushArrayList();
+        if (toothbrushes == null) {
+            return false;
+        }
+        ArrayList<HistoryUse> usesAll = new ArrayList<>();
+        for (Toothbrush toothbrush : toothbrushes) {
+            if (toothbrush == null) {
+                continue;
+            }
+            List<HistoryUse> historyUses = toothbrush.getHistoryUseArrayList();
+            usesAll.addAll(historyUses);
+        }
+        Collections.sort(usesAll, new Comparator<HistoryUse>() {
+            @Override
+            public int compare(HistoryUse o1, HistoryUse o2) {
+                return o1.getDate().compareTo(o2.getDate());
+            }
+        });
+
+        ArrayList<HistoryUse> usesInGoal = new ArrayList<>();
+        Date endDate = new Date();
+        for (HistoryUse use : usesAll) {
+            if (use.getDate().compareTo(startDate) >= 0 && use.getDate().compareTo(endDate) <= 0) {
+                usesInGoal.add(use);
+            } else if (use.getDate().compareTo(endDate) > 0) {
+                break;
+            }
+        }
+
+        if (usesInGoal.size() < days) {
+            return false;
+        }
+
+        int continuousDays = 0;
+        Date dateStart = startDate;
+        Date dateEnd = DateSelector.getEndTimeThisDay(dateStart);
+        for (HistoryUse use : usesAll) {
+            if (use.getDate().compareTo(dateStart) >= 0 && use.getDate().compareTo(dateEnd) <= 0) {
+                continuousDays++;
+                dateStart = DateSelector.getStartTimeThisDay(DateSelector.getDaysAfter(dateStart, 1));
+                dateEnd = DateSelector.getEndTimeThisDay(dateStart);
+            }
+            if (continuousDays >= days) {
+                return true;
+            }
+        }
+        return continuousDays >= days;
+    }
+
 }
--- app/src/main/java/org/ihci/itbs/view/GoalView.java
+++ app/src/main/java/org/ihci/itbs/view/GoalView.java
@@ -9,13 +9,17 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.ihci.itbs.R;
 import org.ihci.itbs.contract.GoalContract;
+import org.ihci.itbs.model.pojo.Award;
 import org.ihci.itbs.model.pojo.Goal;
 import org.ihci.itbs.presenter.GoalPresenter;
 import org.ihci.itbs.view.adapter.GoalAdapter;
+import org.ihci.itbs.widget.AwardDialog;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -41,6 +45,55 @@ public class GoalView extends Activity implements GoalContract.View {
     @Override
     public void showGoalList(List<Goal> goals) {
 
+    }
+
+    @Override
+    public void notifyGoalFinish(final int star, final Award award) {
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewGoalList);
+        for (int i = 0; i < recyclerView.getAdapter().getItemCount(); ++i) {
+            ConstraintLayout layout = (ConstraintLayout) recyclerView.getChildAt(i);
+            if (layout == null) {
+                return;
+            }
+            Button buttonSetGoal = layout.findViewById(R.id.buttonSetGoal);
+            TextView textViewGoalDescription = layout.findViewById(R.id.textViewGoalDescription);
+            int goalId;
+            try {
+                goalId = Integer.parseInt(textViewGoalDescription.getHint().toString());
+            } catch (NumberFormatException e) {
+                goalId = 0;
+            }
+            if (goalId == 0) {
+                buttonSetGoal.setVisibility(View.GONE);
+                continue;
+            }
+            Goal userCurrentGoal = goalPresenter.getUserGoal();
+            if (userCurrentGoal != null && userCurrentGoal.getGoalId() == goalId) {
+                buttonSetGoal.setBackground(getDrawable(R.drawable.radius_rect_button_selector));
+                buttonSetGoal.setText("领取奖励");
+                buttonSetGoal.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        goalPresenter.userFinishGoal(star, award);
+
+                        ArrayList<Award> awardList = new ArrayList<>(1);
+                        awardList.add(award);
+                        AwardDialog awardDialog = new AwardDialog(GoalView.this, star, awardList);
+                        awardDialog.create();
+                        awardDialog.show();
+
+                        initRecyclerView();
+                    }
+                });
+            } else {
+                buttonSetGoal.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getApplicationContext(), "请先领取已完成目标的奖励", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
     }
 
     public void onClickBack(View view) {
@@ -69,6 +122,7 @@ public class GoalView extends Activity implements GoalContract.View {
             @Override
             public void run() {
                 initRecyclerViewItem();
+                goalPresenter.checkIsGoalFinish();
             }
         }, 20);
     }
